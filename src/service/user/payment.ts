c33588331bca4a36693d5688b978052d9dc6e7ab
appitsolution <applicationitsolution@gmail.com>
2023-04-27 19:09:58 +0300

    test

--- src/service/user/payment.ts
+++ src/service/user/payment.ts
@@ -1,8 +1,9 @@
 const payment = async (req, res) => {
-  console.log(res);
   console.log(req.params);
-  console.log(req.quary);
+  console.log(req.query);
   console.log(req.body);
+
+  console.log(req.headers);
   res.status(200).send("ok");
 };
 
